5bc77dbd7196e5ffdf8c049dc29d19fa223a6dbc
aa <none@none.com>
2020-11-15 01:37:10 +0100

    Switched from chasing a random path to chasing the cursor to generate training data.

--- data_generator.py
+++ data_generator.py
@@ -3,79 +3,67 @@ import os
 import numpy as np
 import cv2
 from tqdm import tqdm
-from misc.eye_extractor import extract_eyes
+from misc.eye_extractor import extract_face
 from datetime import datetime
 
-def points_to_path(points, num_points=50):
-    path = []
-    for i in range(1, len(points)):
-        d = points[i-1]
-        path += list(np.linspace(points[i-1], points[i], num_points).astype(int))
-    return path
+class CursorChaser():
+    def __init__(self, w=1920, h=1080, s=3):
+        self.bg = np.zeros((h,w,3))
+        self.w = w
+        self.h = h
+        self.s = s
 
-def run_point_chasing(w=1920, h=1080, s=10, s_2=5, look_ahead_len=20):
+        cv2.namedWindow('CC',cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty('CC',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        cv2.setMouseCallback('CC',self.update)
+        self.cam = cv2.VideoCapture(0)
 
-    w, h = 1920, 1080
-    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-    cam = cv2.VideoCapture(0)
+        self.cursor_positions = []
+        self.images = []
+        self.start()
 
-    points = [[  s,  s],
-              [h-s,  s],
-              [h-s,w-s],
-              [  s,w-s],
-              [  s,  s]]
-    points += list(np.vstack([np.random.randint(s, h-s, 16),
-                              np.random.randint(s, w-s, 16)]).T)
-    path = points_to_path(points)
-    images = []
-    for i,(y,x) in enumerate(tqdm(path)):
+    def start(self):
+        while(1):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.cam.release()
+                cv2.destroyAllWindows()
+                return
 
-        # Black background
-        img = np.zeros((int(h),int(w),3))
-
-        # *Look ahead* fading trail in red
-        for j,(yy,xx) in enumerate(path[i+1:i+look_ahead_len]):
-            img[yy-s_2:yy+s_2,xx-s_2:xx+s_2,2] = 0.99*(1./(j+1))
-
-        # Green dot to follow
-        img[y-s:y+s,x-s:x+s,1] = 1
-
-        cv2.imshow("window", img)
+    def update(self, event,x,y,flag,param):
+        self.bg *= 0.95
+        s = self.s
+        self.bg[y-s:y+s,x-s:x+s,1] = 1.
+        cv2.imshow('CC', self.bg)
 
         # Capture current 'face state' i.e. camera input
-        _, img = cam.read()
-        images.append(img)
+        _, img = self.cam.read()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            exit()
+        self.cursor_positions.append((x,y))
+        self.images.append(img)
 
-    cam.release()
-    cv2.destroyAllWindows()
+def extract_faces(images, cursor_positions, img_shape): # TODO can be batched
 
-    return images, path
+    faces, labels = [], []
+    for i in tqdm(range(len(images))):
+        f = extract_face(images[i], resize_to=img_shape)
+        if f is not False: # if a face was detected
+            labels.append(cursor_positions[i])
+            faces.append(f)
+    return faces, labels
 
-def extract_both_eyes(images, resize_to=None):
-    combined_eyes = []
-    for img in tqdm(images):
-        #TODO this should be batched
-        eyes = extract_eyes(img, resize_to=resize_to)
-        combined_eyes.append(np.hstack(eyes))
-    return combined_eyes
+def save_data(root, instances, faces, labels):
 
-def save_data(eyes, labels, root, instance):
-
-    img_path = f'{root}/images/{instance}'
+    img_path = f'{root}/faces/{instance}'
     os.makedirs(img_path, exist_ok=True)
 
     labels_path = f'{root}/labels/{instance}.txt'
     labels_file = open(labels_path, "a")
 
-    for i,(y,x) in enumerate(tqdm(path)):
+    for i,(x,y) in enumerate(tqdm(labels)):
 
         # Writing image
         img_name = str(i).zfill(5)
-        cv2.imwrite(f'{img_path}/{img_name}.jpg', eyes[i])
+        cv2.imwrite(f'{img_path}/{img_name}.jpg', faces[i])
 
         # Write label
         labels_file.write(f'{img_name},{x},{y}\n')
@@ -93,9 +81,13 @@ if __name__ == '__main__':
     print(f'Instance: {instance}')
     print(f'Assumed resolution: {w}x{h}')
     print('-'*40)
-    print(f'Running point chaser:')
-    images, path = run_point_chasing(w=w, h=h)
-    print(f'Extracting eyes:')
-    eyes = extract_both_eyes(images, resize_to=(30,20))
+
+    print(f'Running cursor chaser:')
+    cc = CursorChaser(w=w,h=h)
+
+    print(f'Running cursor chaser:')
+    img_shape = (100,100)
+    faces, labels = extract_faces(cc.images, cc.cursor_positions, img_shape=img_shape)
+
     print(f'Saving data to {root}/:')
-    save_data(eyes, path, root, instance)
+    save_data(root, instance, faces, labels)
